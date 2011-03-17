2d50724c6aecc9e69e756d512c0eda7b4e669da0
Antoine Albertelli <antoine.albertelli@gmail.com>
2011-03-17 16:14:34 +0100

    Enleve plein de warnings tout mechants pas beaux.

--- modules/cvra_bldc/cvra_bldc.c
+++ modules/cvra_bldc/cvra_bldc.c
@@ -3,22 +3,22 @@
 #include <cvra_bldc.h>
 
 /** Resets the device by setting all registers to 0. */
-void cvra_bldc_reset(cvra_bldc device) {
-    IOWR(device, BLDC_CNT_REGISTER_OFFSET, 0);
-    IOWR(device, BLDC_PWM_REGISTER_OFFSET, BLDC_PWM_MINIMAL_VALUE);
+void cvra_bldc_reset(void * device) {
+    IOWR((int *)device, BLDC_CNT_REGISTER_OFFSET, 0);
+    IOWR((int *)device, BLDC_PWM_REGISTER_OFFSET, BLDC_PWM_MINIMAL_VALUE);
 }
 
 /** Sets the PWM. */
-void cvra_bldc_set_pwm(cvra_bldc device, int32_t value) {
+void cvra_bldc_set_pwm(void * device, int32_t value) {
     if(value < BLDC_PWM_MINIMAL_VALUE && value > -BLDC_PWM_MINIMAL_VALUE)
         value = BLDC_PWM_MINIMAL_VALUE;
 
-    IOWR(device, BLDC_PWM_REGISTER_OFFSET, value);
+    IOWR((int *)device, BLDC_PWM_REGISTER_OFFSET, value);
 }
 
 /** Gets encoder. */
-int32_t cvra_bldc_get_encoder(cvra_bldc device) {
+int32_t cvra_bldc_get_encoder(void * device) {
     volatile int32_t tmp;
-    tmp = IORD(device, BLDC_CNT_REGISTER_OFFSET);
+    tmp = IORD((int *)device, BLDC_CNT_REGISTER_OFFSET);
     return tmp;
 }
