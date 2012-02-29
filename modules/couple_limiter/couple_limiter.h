645e163f29807244150c7fb6cb03c163582ec731
Antoine Albertelli <antoine.albertelli@gmail.com>
2012-02-29 11:27:29 +0100

    Ajout d'un peu de doc au cas ou qqn devrait reprendre le code.

--- modules/couple_limiter/couple_limiter.h
+++ modules/couple_limiter/couple_limiter.h
@@ -1,8 +1,12 @@
-/*
- * couple_limiter.h
- *
- *  Created on: 8 févr. 2012
- *      Author: Antoine Albertelli
+/** @file couple_limiter.h
+ * 
+ * @brief Ce module est charge de limiter le couple induit par les roues
+ * pour eviter que nos pneus fragiles s'abiment.
+ * 
+ * @date 8.2.2012
+ * @author Antoine Albertelli
+ * @todo Il faudra peut etre ajouter des coef de bricolage sur le calcul
+ * du couple.
  */
 
 #ifndef COUPLE_LIMITER_H_
@@ -11,16 +15,23 @@
 #include <aversive.h>
 #include <control_system_manager.h>
 
+/** Structure contenant toutes les variables utiles a ce module. */
 struct couple_limiter {
 	int32_t couple_limit;
 	int32_t old_feedback; // pour calculer la vitesses
 	struct cs *related_cs;
 };
 
+/** Reset le module */
 void couple_limiter_init(struct couple_limiter *c);
+
+/** Fais une iteration du filtre. */
 int32_t couple_limiter_do_filter(void *v, int32_t in);
 
+/** Set la limite de couple. */
 void couple_limiter_set_limit(struct couple_limiter *c, int32_t limit);
+
+/** Set la boucle de controle a limiter. */
 void couple_limiter_set_related_cs(struct couple_limiter *c, struct cs *r);
 
 #endif /* COUPLE_LIMITER_H_ */
