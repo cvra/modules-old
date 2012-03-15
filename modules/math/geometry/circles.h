48a3498fabe8a8786dfe127d2e3cf21b5ff3bae7
Antoine Albertelli <antoine.albertelli@gmail.com>
2012-03-15 08:30:36 +0100

    Documentation du module de geometrie : DONE. Documentation des fonctions des bras : DONE. Ajout des fichiers doxygen au hgignore : DONE.

--- modules/math/geometry/circles.h
+++ modules/math/geometry/circles.h
@@ -35,19 +35,20 @@ typedef struct _circle {
 	double r;
 } circle_t;
 
-/* return true if a point is in the disc */
+/** Checks if a point is inside a circle.
+ * @return 1 if p is inside c, 0 otherwise.
+ */
 uint8_t pt_is_inside_circle(const point_t *p, circle_t *c);
 
-/*
- * return values:
- *  0 dont cross
- *  1 one intersection point
- *  2 two intersection points
- *
- *  p1, p2 arguments are the crossing points coordinates. Both p1 and
- *  p2 are dummy for 0 result. When result is 1, p1 and p2 are set to
- *  the same value.
- */
+/** Checks if 2 circles are intersecting. 
+  * @return The number of intersection points (0, 1 or 2)
+  * @param [in] c1 The first circle.
+  * @param [in] c2 The second circle. 
+  * @param [out] The first intersection point.
+  * @param [out] The second intersection point.
+  * 
+  * @note If the 2 circles intersect on 1 point, then p1 == p2.
+  */
 uint8_t circle_intersect(const circle_t *c1, const circle_t *c2,
 			 point_t *p1, point_t *p2);
 
--- modules/math/geometry/polygon.h
+++ modules/math/geometry/polygon.h
@@ -22,7 +22,8 @@
 #ifndef _POLYGON_H_
 #define _POLYGON_H_
 
-/** \addtogroup Geometrie */
+/** \addtogroup Geometrie
+ * @{ */
 
 typedef struct _poly {
 	point_t * pts;
@@ -31,45 +32,33 @@ typedef struct _poly {
 
 /* XXX add const, fix arg order */
 
-/*
- * return:
- *  0 not inside
- *  1 inside
- *  2 on edge
- */
+/** Checks if a point belongs to a polygon
+ * @return 0 if outside, 1 if inside, 2 if on edge. */
 uint8_t is_in_poly(const point_t *p, poly_t *pol);
 
-/*
- * public wrapper for is_in_poly()
- *  0 not inside
- *  1 inside
- *  2 on edge
- */
+/** Checks if a point belongs to a polygon
+ * @return 0 if outside, 1 if inside, 2 if on edge. */
 uint8_t is_point_in_poly(poly_t *pol, int16_t x, int16_t y);
 
-/* Is segment crossing polygon? (including edges)
- *  0 don't cross
- *  1 cross
- *  2 on a side
- *  3 touch out (a segment boundary is on a polygon edge, 
- *  and the second segment boundary is out of the polygon)
- *  Fill the intersect_pt if not NULL.
- */
+ /** Checks if a segment is crossing a polygon 
+  * @param [out] intersect_pt Contains the intersection point.
+  * @returns 0 dont cross, 1 cross, 2 on a side,
+  *  3 touch out (a segment boundary is on a polygon edge, 
+  *  and the second segment boundary is out of the polygon) */
 uint8_t 
 is_crossing_poly(point_t p1, point_t p2, point_t *intersect_pt,
 		 poly_t *pol);
 
-/*
- * set coords of bounding box.
- */
+/** Set coordinates of bounding box. */
 void polygon_set_boundingbox(int32_t x1, int32_t y1, int32_t x2, int32_t y2);
 
-/* 
- * return 1 if a point is in the bounding box.
- */
+/** Checks if a point is in the bounding box.
+ * @return 1 if p is in the bounding box. */
 uint8_t is_in_boundingbox(const point_t *p);
 
-/* Giving the list of poygons, compute the graph of "visibility rays".
+/** @brief Constructs the visibility ray graph.
+ * 
+ *  Giving the list of poygons, compute the graph of "visibility rays".
  * This rays array is composed of indexes representing 2 polygon
  * vertices that can "see" each others:
  *
@@ -88,11 +77,11 @@ uint8_t is_in_boundingbox(const point_t *p);
 uint8_t 
 calc_rays(poly_t *polys, uint8_t npolys, uint8_t *rays);
 
-/* Compute the weight of every rays: the length of the rays is used
+/** Compute the weight of every rays: the length of the rays is used
  * here. 
  *
- * Note the +1 is a little hack to introduce a preference between to
- * possiblity path: If we have 3 checpoint aligned in a path (say A,
+ * @note The +1 is a little hack to introduce a preference between two
+ * possiblity path: If we have 3 checkpoints aligned in a path (say A,
  * B, C) the algorithm will prefer (A, C) instead of (A, B, C) */
 void 
 calc_rays_weight(poly_t *polys, uint8_t npolys, uint8_t *rays, 
