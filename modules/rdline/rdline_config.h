7c5926a5b243418c17e275770c09ee5742fcf8be
Antoine Albertelli <antoine.albertelli@gmail.com>
2012-10-09 19:21:55 +0200

    Added module rdline, used to build a command line interface.

---
+++ modules/rdline/rdline_config.h
@@ -0,0 +1,20 @@
+#ifndef _RDLINE_CONFIG_H_
+#define _RDLINE_CONFIG_H_
+
+
+/** @define CONFIG_MODULE_RDLINE_HISTORY
+ * @brief Enables history.
+ * 
+ * If this is defined, the rdline module supports history via arrow up/down.
+ */
+#define CONFIG_MODULE_RDLINE_HISTORY
+
+
+/** @define CONFIG_MODULE_RDLINE_KILL_BUF
+ * @brief Enables cut paste.
+ *
+ * If this is defined, the rdline module supports bash style cut/paste using Ctrl-K and Ctrl-Y.
+ */
+#define CONFIG_MODULE_RDLINE_KILL_BUF
+
+#endif
