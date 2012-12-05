011a3888ee4a5501a0070be190e53ba240cb30ef
Pius von Däniken <pius.vondaeniken@epfl.ch>
2012-12-05 20:57:18 +0100

    translated some comments in source files

--- modules/uptime/uptime.h
+++ modules/uptime/uptime.h
@@ -1,22 +1,21 @@
 #ifndef _UPTIME_H_
 
 
-#include <aversive.h>
+#include <aversive.h> 
 
-/** Retourne l'uptime. 
+/** Returns uptime of the robot
+ * 
+ * Returns time passed since booting the system with a resolution of 1us. This time is counted
+ * by a hardware-timer, so there is no precision drop when the CPU is heavily loaded.
  *
- * Retourne le temps depuis le boot du systeme avec une resolution de 1us. Ce temps est compte par un timer
- * hardware, ce qui garantit la precision meme sous une forte charge CPU.
+ * @note: the value is stored on 32 bit so the counter overflows after ~1h 11min
+ * Therefore this module is not intended to be an RTC but works well as a general purpose timer
  *
- * @note La valeur est stockee sur 32 bits, soit 1h et 11 minutes avant overflow.
- * Ce module n'est donc pas adapte pour faire une RTC mais fonctionne bien comme
- * general purpose timer.
+ * @note: the function triggers a bus transaction (bypassing the cache) which may impact performance
  *
- * @note Cette fonction declenche une transaction sur le bus (bypass du cache) ce qui
- * peut avoir des impacts sur les performances.
- *
- * @todo Cette fonction retourne toujours zero sur simulateur.
+ *@todo: returns zero when run in simulation
  */ 
+
 int32_t uptime_get(void);
 
 #endif
