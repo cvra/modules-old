dfc415acd420c4a6e52a375f3c1296fe21e99dcb
Antoine Albertelli <antoine.albertelli@gmail.com>
2013-02-04 13:39:14 +0100

    Added some tools to build the documentation of the projet and prettyprint warnings.

---
+++ tools/build_doc.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+YELLOW = '\033[1;33m'
+WHITE = '\033[1;m'
+BLUE = '\033[1;34m'
+
+def yellow(s):
+    """Returns a version of s that would be printed in yellow in a shell."""
+    return (YELLOW+s+WHITE)
+
+def blue(s):
+    """Returns a version of s that would be printed in blue."""
+    return (BLUE+s+WHITE)
+
+
+warnings = []
+
+print("Generating documentation, it could take a few minutes...")
+proc = subprocess.Popen("doxygen", shell=True, stderr=subprocess.PIPE)
+
+print("Parsing output...")
+for line in proc.stderr:
+    line = str(line, "utf-8")
+    if "warning" in line:
+        # Parse the line to extract filename and line number
+        a = line.split(":")
+        filename = a[0].split("/")[-1]
+        warnings.append(yellow(filename)+":"+blue(a[1])+":".join(a[2:]))
+
+print("".join(warnings))
+print("done")
--- tools/readme.txt
+++ tools/readme.txt
@@ -4,3 +4,5 @@ dev du robot au sein du club.
 Contenu du dossier :
 - serial-to-tcp.py : Simple script a faire tourner sur le PC embarque qui
   fait un simple pont pour faire du debug over wifi. Quick and dirty, by Antoine
+- build_doc.py : Script generant la doc et parsant les warnings de doxygen
+  pour les rendre plus lisibles.
