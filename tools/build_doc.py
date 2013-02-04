d2da48922f9669cd9ebde4bc7ba90fcbaed2fb26
Antoine Albertelli <antoine.albertelli@gmail.com>
2013-02-04 17:47:13 +0100

    Changed the build_doc script to make it sort warnings by files.

--- tools/build_doc.py
+++ tools/build_doc.py
@@ -6,6 +6,7 @@ YELLOW = '\033[1;33m'
 WHITE = '\033[1;m'
 BLUE = '\033[1;34m'
 RED = '\033[1;31m'
+GREEN = '\033[1;32'
 
 def yellow(s):
     """Returns a version of s that would be printed in yellow in a shell."""
@@ -16,19 +17,33 @@ def blue(s):
     return (BLUE+s+WHITE)
 
 
-warnings = []
+warnings = {}
 
 print("Generating documentation, it could take a few minutes...")
 proc = subprocess.Popen("doxygen", shell=True, stderr=subprocess.PIPE)
 
-print("Parsing output...")
 for line in proc.stderr:
-    line = str(line, "utf-8")
+    line = str(line, "utf-8").strip()
     if "warning" in line:
-        # Parse the line to extract filename and line number
-        a = line.split(":")
-        filename = a[0].split("/")[-1]
-        warnings.append(yellow(filename)+":"+blue(a[1])+":".join(a[2:]))
+        (filename, line, message) = line.split("|")
+        if filename in warnings:
+            warnings[filename].append((line, message))
+        else:
+            warnings[filename] = [(line, message)]
 
-print("".join(warnings))
-print(RED+"{0} warnings !!".format(len(warnings))+WHITE)
+
+warning_count = 0
+for filename, messages in sorted(warnings.items()):
+    print("{0}{1}{2}, {3} warnings".format(YELLOW, filename, WHITE, len(messages)))
+    for l, m in messages:
+        warning_count = warning_count + 1
+        print("  {0}{1}{2} : {3}".format(BLUE, l, WHITE, m))
+
+
+print("{0}{1} warnings{2}".format(warning_count==0 and GREEN or RED, warning_count, WHITE))
+    
+
+# Opens the result in a web browser
+subprocess.Popen("xdg-open ./auto_doc/html/index.html", shell=True)
+
+
