8793ddf1ab8412333487476cb4052d4398101eaa
Antoine Albertelli <antoine.albertelli@gmail.com>
2013-03-07 00:36:15 +0100

    Added the ramp filter to do speed control systems.

---
+++ modules/ramp/ramp.c
@@ -0,0 +1,64 @@
+/*  
+ *  Copyright Droids Corporation, Microb Technology, Eirbot (2005)
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *
+ */
+
+
+#include <aversive.h>
+#include "ramp.h"
+
+
+
+void ramp_init(struct ramp_filter * r) {
+  r->var_neg = 0xFFFFFFFF;
+  r->var_pos = 0xFFFFFFFF;
+  r->prev_out = 0; 
+}
+
+void ramp_set_vars(struct ramp_filter * r, uint32_t neg, uint32_t pos) {
+  r->var_neg = neg;
+  r->var_pos=pos;
+}
+
+/*Filter the in value using the ramp_filter r*/
+int32_t ramp_do_filter(void * data, int32_t in) {
+    uint32_t variation;
+    struct ramp_filter * r = (struct ramp_filter *) data;
+
+    /* test if the variation is positive or negative */
+    if(in > r->prev_speed) {
+        variation = in-r->prev_speed;
+        /* test if the variation is too high */
+        if(variation < r->var_pos)
+            r->prev_speed = in; 
+        else
+            r->prev_speed = r->prev_speed + r->var_pos; 
+    }
+    else {
+        variation = r->prev_speed-in;
+        if(variation < r->var_neg)
+            r->prev_speed = in; 
+        else
+            r->prev_speed = r->prev_speed - r->var_neg;
+    }
+
+    r->prev_out += r->prev_speed;
+    return r->prev_out;
+}
+
+
---
+++ modules/ramp/ramp.h
@@ -0,0 +1,76 @@
+/*  
+ *  Copyright Droids Corporation, Microb Technology, Eirbot (2005)
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *
+ */
+
+
+/** @file ramp.h
+ * @brief This modules provides a speed ramp.
+ *
+ * This modules is used to do a speed regulation on top of a position
+ * based system. This functions outputs a position value, moving at the given
+ * speed an therefore achieving a speed regulation. This technique is used by
+ * the LM629 modules so it should not be too bad.
+ *
+ * @todo Test this module, should work though.
+ */
+
+
+
+#ifndef _RAMP_H_
+#define _RAMP_H_
+
+/** Instance of the ramp filter. */
+struct ramp_filter {
+	uint32_t var_neg;   /**< Maximal negative variation. */        
+	uint32_t var_pos;   /**< Maximal positive variation. */ 
+    int32_t prev_speed; /**< Speed at the previous iteration. */
+	int32_t prev_out;   /**< Previous position output. */
+};
+
+
+
+/** @brief Initialize a ramp instance.
+ *
+ * This function initializes the ramp filter, setting all values to 0.
+ * @param [in,out] r The ramp instance to initialize.
+ */
+void ramp_init(struct ramp_filter * r );
+
+
+/** @brief Set acceleration.
+ *
+ * This function sets the maximal acceleration, both negative and positive.
+ * @param [in,out] r The ramp instance.
+ * @param [in] neg Maximal deceleration.
+ * @param [in] pos Maximal acceleration.
+ */
+void ramp_set_vars(struct ramp_filter * r, uint32_t neg, uint32_t pos);
+
+
+/** @brief Do filter.
+ *
+ * This function computes the new speed while respecting maximal acceleration,
+ * and outputs the new position consign.
+ * @param [in] r The ramp instance, cast to void * to be compatible with CSM.
+ * @param [in] in The speed consign.
+ * @returns The computed position consign.
+ */
+int32_t ramp_do_filter(void * r, int32_t in);
+
+#endif /* ifndef _RAMP_H */
