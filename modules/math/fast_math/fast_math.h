7567c665b93e27290817992c4f870e6af604685d
Mathieu Rouvinez <devnull@localhost>
2013-03-17 19:00:25 +0100

    Profound update of the fast_math library.

--- modules/math/fast_math/fast_math.h
+++ modules/math/fast_math/fast_math.h
@@ -21,30 +21,60 @@ float fast_fabsf    (float v);
 /** Computes a sine.
  *
  * This function computes a sine by using a taylor series expansion.
+ * Validity range: [-pi,pi]
  */
 float fast_sinf     (float v);
 
 /** Computes a cosine.
  *
- * This function computes a cosine by using a taylor series expansion.
+ * This function computes a cosine by using a Taylor series expansion.
+ * Validity range: [-pi,pi]
  */
 float fast_cosf     (float v);
 
+/** Computes a tangent.
+ *
+ * This function computes a tangent by using a lookup table.
+ */
+float fast_tanf     (float v);
+
+/** Computes arcsine.
+ *
+ * This function computes an arcsine by using a lookup table.
+ */
+float fast_asinf    (float v);
+
+/** Computes an arccosine.
+ *
+ * This function computes an arccosine by using a lookup table.
+ */
+float fast_acosf    (float v);
+
+/** Computes an arctangent.
+ *
+ * This function computes an arctangent by using a lookup table.
+ */
+float fast_atanf    (float v);
+
 /** Computes an atan2.
  *
- * This function computes an atan2 by using a taylor series expansion.
+ * This function computes an atan2 by using a Taylor series expansion.
  */
 float fast_atan2f   (float y, float x);
 
 /** Computes a square root
  *
- * This function computes a square root by using a fast lookup table. */
+ * This function computes a square root by using a fast lookup table (Hsieh).
+ */
 float fast_sqrtf    (float v);
 
-/** Computes the inverse of a square root using Carmack's trick. */
+/** Computes the inverse of a square root using Carmack's trick (Lomont). */
 float fast_invsqrtf (float v);
 
+/** Computes the inverse of a float using a very rough approximation. */
+float fast_invf     (float v);
+
 /** Benchmarks the library. */
-void fast_benchmark(void);
+void  fast_benchmark(void);
 
 #endif
