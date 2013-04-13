5bc094c628431f482b3213d0eab12cc2050b2998
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2013-04-13 18:26:20 +0200

    Working on avoiding

--- modules/cvra_beacon/cvra_beacon.c
+++ modules/cvra_beacon/cvra_beacon.c
@@ -57,7 +57,7 @@ void cvra_beacon_init(cvra_beacon_t *beacon, void *adress, int irq_number)
     }
     
     /* Start motor with 50% PWM */
-    IOWR(beacon->beacon_adress, PWM_REGISTER, 127);
+    IOWR(beacon->beacon_adress, PWM_REGISTER, 90);
     
     /* Store actuel index */
     beacon->lastindex = IORD(beacon->beacon_adress, INDEX_REGISTER);
