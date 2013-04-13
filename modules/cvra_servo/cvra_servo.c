0c2a83b444265e7064ec0991dee45510aec6c449
Florian Reinhard <florian.reinhard@epfl.ch>
2013-04-13 11:00:03 +0200

    still strat test

--- modules/cvra_servo/cvra_servo.c
+++ modules/cvra_servo/cvra_servo.c
@@ -1,6 +1,6 @@
 #include <cvra_servo.h>
 
-void cvra_servo_set(void *base, int channel, int32_t value) {
+void cvra_servo_set(void *base, int channel, uint32_t value) {
     if(channel < 0 || channel > 3)
         return;
 
--- modules/cvra_servo/cvra_servo.h
+++ modules/cvra_servo/cvra_servo.h
@@ -5,6 +5,6 @@
 
 /** Sets the servo pulse width.
  * @todo Can we set pulse time > 20ms ? What happens then ? */
-void cvra_servo_set(void *base, int channel, int32_t value);
+void cvra_servo_set(void *base, int channel, uint32_t value);
 
 #endif
