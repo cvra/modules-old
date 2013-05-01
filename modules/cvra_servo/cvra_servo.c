a163c3ed72b97af3e8ac1a532bc4e8b920f35a78
Florian Reinhard <florian.reinhard@epfl.ch>
2013-05-01 20:20:23 +0200

    added functions that can be used by a PID filter

--- modules/cvra_servo/cvra_servo.c
+++ modules/cvra_servo/cvra_servo.c
@@ -6,3 +6,15 @@ void cvra_servo_set(void *base, int channel, uint32_t value) {
 
     IOWR(base, channel, value);
 }
+
+void cvra_servo_set0(void *base, uint32_t value){
+    cvra_servo_set(base, 0, value);
+}
+
+void cvra_servo_set1(void *base, uint32_t value){
+    cvra_servo_set(base, 1, value);
+}
+
+void cvra_servo_set2(void *base, uint32_t value){
+    cvra_servo_set(base, 2, value);
+}
