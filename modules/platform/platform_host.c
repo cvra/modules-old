52448c0aa944c47458def1d55a597166230de77a
Antoine Albertelli <antoinea101@gmail.com>
2014-03-20 11:34:56 +0100

    Added abstraction of operating system semaphores.

--- modules/platform/platform_host.c
+++ modules/platform/platform_host.c
@@ -1,6 +1,7 @@
 #include <platform.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 void reset(void)
@@ -13,6 +14,26 @@ void panic(void)
 {
     printf("%s()\n", __FUNCTION__);
     exit(EXIT_FAILURE);
+}
+
+void platform_create_semaphore(semaphore_t *sem, int count)
+{
+    sem->count = count;
+    sem->acquired_count = 0;
+}
+
+
+void platform_signal_semaphore(semaphore_t *sem)
+{
+    sem->count++;
+}
+
+void platform_take_semaphore(semaphore_t *sem)
+{
+    if (sem->count > 0) {
+        sem->count--;
+        sem->acquired_count++;
+    }
 }
 
 
--- modules/platform/platform_host.h
+++ modules/platform/platform_host.h
@@ -9,4 +9,13 @@
 /** Reimplementation de IOWR du NIOS2 pour compiler sur le PC */
 #define IOWR(adress, offset, data) (*((int32_t *)adress+offset) = data)
 
+typedef struct {
+    int count;
+    int acquired_count;
+} semaphore_t;
+
+void platform_create_semaphore(semaphore_t *sem, int count);
+void platform_signal_semaphore(semaphore_t *sem);
+void platform_take_semaphore(semaphore_t *sem);
+
 #endif
